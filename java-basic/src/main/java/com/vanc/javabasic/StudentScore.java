130c979c3c0cbfbf97cc71ef3b6e982a486c14e3
Vanc <vanc.song@wetax.com.cn>
2020-07-08 17:16:29 +0800

    【add】java8 lambda map的一些用法,策略模式 逻辑分派

---
+++ java-basic/src/main/java/com/vanc/javabasic/StudentScore.java
@@ -0,0 +1,14 @@
+package com.vanc.javabasic;
+
+import lombok.Data;
+
+/**
+ * @author : vanc.song@wetax.com.cn
+ * @date: 2020-07-08 14:32
+ */
+@Data
+public class StudentScore {
+	private String stuName;
+	private String subject;
+	private Integer score;
+}
